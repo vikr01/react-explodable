e80f97b108e4fff52b7ce5a845c9a03e394e4d50
vikr01 <vik120@icloud.com>
2025-05-25 01:34:05 -0700

    more refactors to example, add start script to root

--- packages/example/src/main.tsx
+++ packages/example/src/main.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.js";
+import App from "./components/App.js";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
